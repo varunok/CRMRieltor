3c39b992f91098e96e53f281252fabb589835602
varunok <varunok13@gmail.com>
2016-08-16 17:47:33 +0300

    change parser

--- homes/static/js/parser_olx.js
+++ homes/static/js/parser_olx.js
@@ -4,7 +4,8 @@
 $(document).ready(function() {
     $('#parse').on('click', function (event) {
         event.preventDefault();
-        $.post('parse')
+        var msg   = $('#parse_form').serialize();
+        $.post('parse', msg)
         .success( function (data) {
             $('#include_parse_text').text(data);
         })
