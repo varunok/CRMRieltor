8e4ba712b6383ca01f5efec93b64fa1e27d42eb2
varunok <varunok13@gmail.com>
2016-09-01 05:22:28 +0300

    modif SQL query in app parsing #7

--- posting/insert_table.py
+++ posting/insert_table.py
@@ -45,7 +45,13 @@ class InsertData():
 
         db = MySQLdb.connect(user=self.textusername, passwd=self.textpassword, host=self.texthostname, db=self.database, autocommit=True)
         c = db.cursor()
-        query = "INSERT INTO Object_Daily (description, contactPerson) VALUES ('%s', '%s')" % (self.data.comment,
-                                                                                               self.data.name_owner)
+        query = "INSERT INTO Object_Daily (description, " \
+                "contactPerson, contactPhone, price, address) " \
+                "VALUES ('%s', '%s', '%s', '%s', '%s')" % \
+                (self.data.comment,
+                 self.data.name_owner,
+                 self.data.phone_owner,
+                 self.data.price_month,
+                 self.data.street_obj+self.data.number_home)
         c.execute(query)
         # c.commit()
