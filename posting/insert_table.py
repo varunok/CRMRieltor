f4fad10b3e1be84e58956965bfc64beea646ad46
varunok <varunok13@gmail.com>
2016-09-07 03:43:09 +0300

    fix posting #12

--- posting/insert_table.py
+++ posting/insert_table.py
@@ -49,7 +49,7 @@ class InsertData():
         query = "INSERT INTO Object_Live (code, address)" \
                 "VALUES ('%s', '%s')" % \
                 (str(self.data.id),
-                 str(self.data.street_obj).decode('utf-8'))
+                 str(self.data.street_obj).encode('utf-8'))
         print(query)
         c.execute(query)
         # c.commit()
