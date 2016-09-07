8df2e7f9985cdefc6d1e41f83ca85eb6184cae61
varunok <varunok13@gmail.com>
2016-09-07 04:46:05 +0300

    fix posting #25

--- posting/insert_table.py
+++ posting/insert_table.py
@@ -49,7 +49,7 @@ class InsertData():
         db = MySQLdb.connect(user=self.textusername, passwd=self.textpassword, host=self.texthostname, db=self.database, autocommit=True)
         c = db.cursor()
         query = "INSERT INTO Object_Live (code, title, description, address)" \
-                "VALUES ('%s', '%s', '%s')" % \
+                "VALUES ('%s', '%s', '%s', '%s')" % \
                 (str(self.data.id),
                  str(self.data.street_obj),
                  str(self.data.comment),
