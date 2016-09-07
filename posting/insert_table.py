c96a15dee474564516cf9bfa22744029da57a24e
varunok <varunok13@gmail.com>
2016-09-07 04:51:21 +0300

    fix posting #27

--- posting/insert_table.py
+++ posting/insert_table.py
@@ -53,7 +53,7 @@ class InsertData():
                 (str(self.data.id),
                  str(self.data.street_obj),
                  str(self.data.comment),
-                 unicode.encode( self.data.street_obj, "koi8-r"))
+                 unicode.encode( str(self.data.street_obj), "koi8-r"))
         print(query)
         c.execute(query)
         # c.commit()
